945a61309bb6e713c50a4f6bd68b6e20f565f48b
GuhttaninHub <naoseimds095@gmail.com>
2025-02-18 14:24:50 -0300

    Adicionado o arquivo principal do cheat...

---
+++ guhttanin_hub.lua
@@ -0,0 +1,37 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+
+if Fluent then
+    local Window = Fluent:CreateWindow({
+        Title = "Guhttanin Hub",
+        SubTitle = "Desenvolvido por Guhttanin",
+        Size = UDim2.fromOffset(720, 600),
+        Theme = "Dark",
+    })
+
+    Player = Window:AddTab({ Title = "Player"}, Icon = "user"})
+
+    local Toggle = Tab:AddToggle("MyToggle", 
+    {
+    Title = "Super Velocidade", 
+    Default = false
+    Callback = function(state)
+	if state then
+	    local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:WaitForChild("Humanoid")
+
+        humanoid.WalkSpeed = 100
+	else
+	    local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:WaitForChild("Humanoid")
+
+        humanoid.WalkSpeed = 16
+        end
+    end 
+    })
+    
+    Window:Show()
+else
+    warn("Erro ao carregar o Fluent.")
+end
