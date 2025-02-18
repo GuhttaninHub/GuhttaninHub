3c9767b7dcd7b01555e6ccea523640578422bfda
GuhttaninHub <naoseimds095@gmail.com>
2025-02-18 20:34:00 -0300

    Update guhttanin_hub.lua

--- guhttanin_hub.lua
+++ guhttanin_hub.lua
@@ -6,6 +6,7 @@ if Fluent then
         SubTitle = "Desenvolvido por Guhttanin",
         Size = UDim2.fromOffset(720, 400),
         Theme = "Dark",
+        Resize = true,
     })
 
     local Player = Window:AddTab({ Title = "Settings", Icon = "user"})
