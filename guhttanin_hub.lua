ce6c7a2d9004f2df85c9a2012a7bc3b93168604a
GuhttaninHub <naoseimds095@gmail.com>
2025-02-18 15:17:18 -0300

    Update guhttanin_hub.lua

--- guhttanin_hub.lua
+++ guhttanin_hub.lua
@@ -10,16 +10,16 @@ if Fluent then
 
     local Player = Window:AddTab({ Title = "Player", Icon = "user"})
 
-    local slider_walkspeed = Player.AddSlider({
+    local slider_walkspeed = Player:AddSlider({
             Title = "Velocidade",
             Min = 0,
             Max = 500,
             Default = 16,
             Increment = 1,
             Callback = function(value)
-                local player = game.Player.LocalPlayer
+                local player = game.Players.LocalPlayer
                 local character = player.Character or player.Character:AddedWait()
-                local humanoid = character.WaitForChild("Humanoid")
+                local humanoid = character:WaitForChild("Humanoid")
 
                 humanoid.WalkSpeed = value
             end
