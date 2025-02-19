a3656b5265f5401c07e9f7799e5ddbfbd9c850a5
GuhttaninHub <naoseimds095@gmail.com>
2025-02-19 11:17:07 -0300

    Update guhttanin_hub.lua

--- guhttanin_hub.lua
+++ guhttanin_hub.lua
@@ -145,7 +145,7 @@ if Fluent then
         for _, player in pairs(game.Players:GetPlayers()) do
             table.insert(players, player.Name)
         end
-        dropdown_tween_player:SetValue(players)
+        dropdown_tween_player:Options(players)
     end
 
     UpdatePlayers()
